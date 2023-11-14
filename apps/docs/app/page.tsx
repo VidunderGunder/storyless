497c270f77110bdeb26cd5ab978a13903a80704c
VidunderGunder <kristiankramas@outlook.com>
2023-11-14 12:35:54 +0100

    Remove broken Icon

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,4 +1,3 @@
-// import { Icon } from "@storyless/react";
 import Image from "next/image";
 import styles from "./page.module.css";
 
@@ -6,7 +5,6 @@ export default function Page(): JSX.Element {
   return (
     <main className={styles.main}>
       <div className={styles.banner}>
-        {/* <Icon className={styles.icon} /> */}
         <Image alt="Storyless Logo" height={150} src="/logo.svg" width={150} />
         <h1 className={styles.h1}>Storyless</h1>
       </div>
