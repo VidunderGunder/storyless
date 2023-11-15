69f7eb6291df7f57d9d5cd9d59834d0ce6eeff25
VidunderGunder <kristiankramas@outlook.com>
2023-11-15 18:38:06 +0100

    Drop Vite support temp

--- packages/storyless-react/index.tsx
+++ packages/storyless-react/index.tsx
@@ -8,13 +8,6 @@ let BUNDLE_STORYLESS =
   process.env.NEXT_PUBLIC_BUNDLE_STORYLESS === "true" ||
   process.env.REACT_APP_BUNDLE_STORYLESS === "true";
 
-try {
-  const importMetaBundle =
-    // @ts-ignore
-    (import.meta?.["env"]?.VITE_BUNDLE_STORYLESS ?? "false") === "true";
-  if (importMetaBundle) BUNDLE_STORYLESS = true;
-} catch (e) {}
-
 export const Storyless: (typeof storyless)["Storyless"] = BUNDLE_STORYLESS
   ? forwardRef(function () {
       return null;
