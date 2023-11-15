0504a79fa38347b824b27c7490c398493945ddfa
VidunderGunder <kristiankramas@outlook.com>
2023-11-15 18:37:17 +0100

    New attempt at env bundling

--- packages/storyless-react/index.tsx
+++ packages/storyless-react/index.tsx
@@ -2,14 +2,21 @@
 import { forwardRef } from "react";
 import * as storyless from "./Storyless";
 
-export const Storyless: (typeof storyless)["Storyless"] =
+let BUNDLE_STORYLESS =
   process.env.NODE_ENV !== "development" ||
   process.env.BUNDLE_STORYLESS === "true" ||
   process.env.NEXT_PUBLIC_BUNDLE_STORYLESS === "true" ||
-  process.env.REACT_APP_BUNDLE_STORYLESS === "true" ||
-  // @ts-ignore
-  (import.meta?.["env"]?.VITE_BUNDLE_STORYLESS ?? "false") === "true"
-    ? forwardRef(function () {
-        return null;
-      })
-    : storyless.Storyless;
+  process.env.REACT_APP_BUNDLE_STORYLESS === "true";
+
+try {
+  const importMetaBundle =
+    // @ts-ignore
+    (import.meta?.["env"]?.VITE_BUNDLE_STORYLESS ?? "false") === "true";
+  if (importMetaBundle) BUNDLE_STORYLESS = true;
+} catch (e) {}
+
+export const Storyless: (typeof storyless)["Storyless"] = BUNDLE_STORYLESS
+  ? forwardRef(function () {
+      return null;
+    })
+  : storyless.Storyless;
