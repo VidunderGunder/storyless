37d2b6f50131ff6e592a8b083cd1c656465d0dd6
VidunderGunder <kristiankramas@outlook.com>
2023-11-15 18:51:41 +0100

    Cleaner env handling

--- packages/storyless-react/index.tsx
+++ packages/storyless-react/index.tsx
@@ -3,13 +3,13 @@ import { forwardRef } from "react";
 import * as storyless from "./Storyless";
 
 let BUNDLE_STORYLESS =
-  process.env.NODE_ENV !== "development" ||
-  process.env.BUNDLE_STORYLESS === "true" ||
-  process.env.NEXT_PUBLIC_BUNDLE_STORYLESS === "true" ||
-  process.env.REACT_APP_BUNDLE_STORYLESS === "true";
+  process.env.BUNDLE_STORYLESS ||
+  process.env.NEXT_PUBLIC_BUNDLE_STORYLESS ||
+  process.env.REACT_APP_BUNDLE_STORYLESS;
 
-export const Storyless: (typeof storyless)["Storyless"] = BUNDLE_STORYLESS
-  ? forwardRef(function () {
-      return null;
-    })
-  : storyless.Storyless;
+export const Storyless: (typeof storyless)["Storyless"] =
+  process.env.NODE_ENV !== "development" || BUNDLE_STORYLESS
+    ? forwardRef(function () {
+        return null;
+      })
+    : storyless.Storyless;
