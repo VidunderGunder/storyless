6257d08876d228f2af93d91a0c96413164af2eda
VidunderGunder <kristiankramas@outlook.com>
2023-11-20 23:38:03 +0100

    Fix lint and type errors

--- apps/feasy/drizzle.config.ts
+++ apps/feasy/drizzle.config.ts
@@ -6,7 +6,8 @@ export default {
   schema: "./src/server/db/schema.ts",
   driver: "mysql2",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    // @ts-expect-error-next-line -- T3 told us to do this, but it could be deprecated soon
+    connectionString: env.DATABASE_URL ?? "",
   },
   tablesFilter: ["feasy-t3-drizzle_*"],
 } satisfies Config;
--- apps/feasy/src/hooks/usePersistentState.tsx
+++ apps/feasy/src/hooks/usePersistentState.tsx
@@ -1,33 +1,33 @@
-import { type Dispatch, type SetStateAction, useEffect, useState } from "react";
+// import { type Dispatch, type SetStateAction, useEffect, useState } from "react";
 
-export function usePersistentState<
-  T extends string | number | boolean | null | undefined,
->(key: string, defaultValue: T): [T, Dispatch<SetStateAction<T>>] {
-  const [state, setState] = useState<T>(() => {
-    if (typeof window === "undefined" || typeof localStorage === "undefined")
-      return defaultValue;
+// export function usePersistentState<
+//   T extends string | number | boolean | null | undefined,
+// >(key: string, defaultValue: T): [T, Dispatch<SetStateAction<T>>] {
+//   const [state, setState] = useState<T>(() => {
+//     if (typeof window === "undefined" || typeof localStorage === "undefined")
+//       return defaultValue;
 
-    const storedValue = localStorage.getItem(key);
+//     const storedValue = localStorage.getItem(key);
 
-    const parsedValue: unknown =
-      typeof storedValue === "string" && storedValue !== "undefined"
-        ? JSON.parse(storedValue)
-        : null;
+//     const parsedValue: unknown =
+//       typeof storedValue === "string" && storedValue !== "undefined"
+//         ? JSON.parse(storedValue)
+//         : null;
 
-    if (
-      typeof parsedValue === "string" ||
-      typeof parsedValue === "number" ||
-      typeof parsedValue === "boolean"
-    )
-      return parsedValue as T;
+//     if (
+//       typeof parsedValue === "string" ||
+//       typeof parsedValue === "number" ||
+//       typeof parsedValue === "boolean"
+//     )
+//       return parsedValue as T;
 
-    return defaultValue;
-  });
+//     return defaultValue;
+//   });
 
-  useEffect(() => {
-    if (typeof window !== "undefined")
-      localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
+//   useEffect(() => {
+//     if (typeof window !== "undefined")
+//       localStorage.setItem(key, JSON.stringify(state));
+//   }, [key, state]);
 
-  return [state, setState];
-}
+//   return [state, setState];
+// }
