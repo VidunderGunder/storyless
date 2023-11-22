221675dc716129bb6df1efae03f49b181f9d91e2
VidunderGunder <kristiankramas@outlook.com>
2023-11-22 01:58:21 +0100

    Cleanup

--- packages/react-storyless/build.ts
+++ packages/react-storyless/build.ts
@@ -1,12 +1,6 @@
 import path from "node:path";
 import packageJson from "./package.json";
 
-// process.env.NODE_ENV = "production";
-// Bun.env.NODE_ENV = "production";
-
-// console.log(process.env.NODE_ENV); // development 👈 culprit?
-// console.log(Bun.env.NODE_ENV); // production
-
 const external = Object.keys(
   packageJson.peerDependencies
 ) as (keyof typeof packageJson.peerDependencies)[];
@@ -15,5 +9,4 @@ await Bun.build({
   entrypoints: [path.join(__dirname, "./index.tsx")],
   outdir: path.join(__dirname, "./build"),
   external,
-  target: "node",
 });
